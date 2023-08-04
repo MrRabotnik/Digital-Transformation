40dd4d9a9dae399349e6f2e4c759a885f7bede92
MrRabotnik <sargis2014@gmail.com>
2023-08-04 19:48:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ let secondSlide = document.getElementById("slideInclusionOnline");
 let thirdSlide = document.getElementById("slideDigitalTransform");
 let fourthSlide = document.getElementById("slideDigitalPlatforms");
 let fifthSlide = document.getElementById("slideSurprise");
+let closeMobileMenu = document.getElementById("closeMobileMenu");
+let header = document.getElementById("header");
+let hamburgerMenu = document.getElementById("hamburgerMenu");
+
 
 nav1.addEventListener("click", () => {
     goToSlide(firstSlide)
@@ -25,6 +29,10 @@ nav5.addEventListener("click", () => {
     goToSlide(fifthSlide)
 })
 
+hamburgerMenu.addEventListener("click", openMobileMenufn)
+
+closeMobileMenu.addEventListener("click", closeMobileMenufn)
+
 function goToSlide(el) {
     let y = el.offsetTop
     window.scrollTo({
@@ -32,3 +40,19 @@ function goToSlide(el) {
         behavior: "smooth",
     });
 }
+
+function openMobileMenufn() {
+    header.style.visibility = "visible"
+    closeMobileMenu.style.visibility = "visible"
+}
+
+function closeMobileMenufn() {
+    header.style.visibility = "hidden"
+    closeMobileMenu.style.visibility = "hidden"
+}
+
+// function resizeScreen(){
+//     header.style.visibility = "visible"
+// }
+
+// document.addEventListener("resize", resizeScreen)
